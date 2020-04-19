8a60fe50dae343abae3741fca1423bbc0c9c3cbe
Garrett Fite <garrett.fite@gmail.com>
2020-04-19 02:55:12 -0500

    WIP Inventory
    Working on Inventory

--- Illuminate/Assets/Resources/Scripts/UIController.cs
+++ Illuminate/Assets/Resources/Scripts/UIController.cs
@@ -1,16 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
-    private Sprite Frame;
+    public GameObject lightBar;
+    public GameObject energyBar;
     public int InventorySlots;
+
+    private PlayerController playerController;
+    private Canvas canvas;
+    private Slider lightLevel;
+    private Slider energyLevel;
+    private Sprite frame;
 
     // Start is called before the first frame update
     void Start()
     {
-        Frame = Resources.Load<Sprite>("Sprites/InventoryFrame");
+        playerController = GetComponent<PlayerController>();
+        lightLevel = lightBar.GetComponent<Slider>();
+        energyLevel = energyBar.GetComponent<Slider>();
+        Sprite frame = Resources.Load<Sprite>("Sprites/InventoryFrame");
         //Test Case, implement with checking inventory capacity of player
         CreateInventoryBar(0, 0);
     }
@@ -18,7 +29,13 @@ public class UIController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Resolving global energy changes in the world
+        UpdateEnergy();
+
+        // Resolving light changes based relative to player
+        UpdateLight();
+        if (lightLevel.value == 0f)
+            playerController.DieToLight();
     }
 
     void CreateInventoryBar(float x, float y)
@@ -28,11 +45,21 @@ public class UIController : MonoBehaviour
             GameObject inventoryFrame = Resources.Load<GameObject>("Prefabs/InventoryFrame");
             inventoryFrame.AddComponent<SpriteRenderer>();
             SpriteRenderer spr = inventoryFrame.GetComponent<SpriteRenderer>();
-            spr.sprite = Frame;
+            spr.sprite = frame;
             inventoryFrame.transform.position = new Vector2(x, y);
             Instantiate(inventoryFrame);
             y += 1.03f;
         }
 
+    }
+
+    private void UpdateEnergy()
+    {
+        energyLevel.value = EnergyHandler.GetFloat();
+    }
+
+    private void UpdateLight()
+    {
+        lightLevel.value = 0.33f;
     }
 }
